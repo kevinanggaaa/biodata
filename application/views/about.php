197e0417ec04c054174895aab5f4df538aef0417
kevinanggaaa <60419316+kevinanggaaa@users.noreply.github.com>
2021-05-23 22:30:08 +0700

    Initial commit

---
+++ application/views/about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="<?php echo base_url('assets/favicon.ico') ?>" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo base_url('css/styles.css') ?>" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- About Section-->
+        <section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">A student of Institut Teknologi Sepuluh Nopember majoring Informatics. I'd love to learn something new and advance my skill to be an expert.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Born in Pekanbaru, 15 April 2000 and live in Pekanbaru, Riau. Currently live in Pakuwon City, Surabaya for studying in Institut Teknologi Sepuluh Nopember</p></div>
+                </div>
+            </div>
+        </section>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo base_url('js/scripts.js') ?>"></script>
+    </body>
+</html>
